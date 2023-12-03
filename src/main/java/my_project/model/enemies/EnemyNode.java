83f179d2d8813b645dcd4d340b89b2c3da1a44a8
Realnr <alexandruttio@gmail.com>
2023-12-03 16:38:42 +0100

    enemy node vorgelegt

--- src/main/java/my_project/model/enemies/EnemyNode.java
+++ src/main/java/my_project/model/enemies/EnemyNode.java
@@ -1,5 +1,16 @@
 package my_project.model.enemies;
 
-public class EnemyNode{
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+public class EnemyNode extends GraphicalObject {
+    @Override
+    public void draw(DrawTool drawTool){
+
+    }
+    @Override
+    public void update(double dt) {
+
+    }
 
 }
