4174286df1872d31f4c176084ddec3577fa63804
goldmaxi <Nordstadt01>
2023-12-04 09:27:44 +0100

    Removed stupid method

--- src/main/java/my_project/Util.java
+++ src/main/java/my_project/Util.java
@@ -99,22 +99,6 @@ public class Util {
     }
 
     /**
-     * source "http://www.jeffreythompson.org/collision-detection/rect-rect.php"
-     */
-
-    public static boolean rToRCollision(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h) {
-        //Ew, fuck you
-        //are the sides of one rectangle touching the other?
-        if (r1x + r1w >= r2x &&    // r1 right edge past r2 left
-                r1x <= r2x + r2w &&    // r1 left edge past r2 right
-                r1y + r1h >= r2y &&    // r1 top edge past r2 bottom
-                r1y <= r2y + r2h) {    // r1 bottom edge past r2 top
-            return true;
-        }
-        return false;
-    }
-
-    /**
      * Checks if two rectangle collide
      * @param r1x x position of left edge from first rectangle
      * @param r1y y position of top edge from first rectangle
