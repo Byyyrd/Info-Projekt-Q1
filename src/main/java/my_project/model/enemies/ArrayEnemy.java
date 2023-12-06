e472cbaee0a9e21e073692a43fb2f2baceea87e9
Realnr <alexandruttio@gmail.com>
2023-12-06 13:16:54 +0100

    Array Enemy und so

--- src/main/java/my_project/model/enemies/ArrayEnemy.java
+++ src/main/java/my_project/model/enemies/ArrayEnemy.java
@@ -1,11 +1,45 @@
 package my_project.model.enemies;
 
+import KAGO_framework.view.DrawTool;
 import my_project.model.Player;
 import my_project.model.Projectile;
 
 public class ArrayEnemy extends Enemy {
-    public ArrayEnemy(double x, double y, double speed, Player player) {
+    private EnemyNode[][] enemyNodesArray;
+    public ArrayEnemy(double x, double y, double speed,double radius, Player player, int ArrayLength) {
         super(x, y, speed, player);
+        this.radius = radius;
+        enemyNodesArray = new EnemyNode[ArrayLength][ArrayLength];
+        initialiseNodes();
+    }
+
+    @Override
+    public void draw(DrawTool drawTool) {
+        drawNodes(drawTool);
+    }
+
+    @Override
+    public void update(double dt) {
+
+    }
+
+    private void initialiseNodes(){
+        double xOrigin =  -enemyNodesArray.length * radius;
+        double yOrigin =  -enemyNodesArray.length * radius;
+        for (int i = 0; i < enemyNodesArray.length; i++) {
+            for (int j = 0; j < enemyNodesArray[i].length ; j++) {
+                double nY = yOrigin - i * radius * 2;
+                double nX = xOrigin + radius * 2 * j;
+                enemyNodesArray[i][j] = new EnemyNode(nX, nY,radius);
+            }
+        }
+    }
+    private void drawNodes(DrawTool drawTool){
+        for (int i = 0; i < enemyNodesArray.length; i++) {
+            for (int j = 0; j < enemyNodesArray[i].length; j++) {
+                drawTool.drawFilledCircle(enemyNodesArray[i][j].getX(),enemyNodesArray[i][j].getY(),enemyNodesArray[i][j].getRadius());
+            }
+        }
     }
 
     @Override
