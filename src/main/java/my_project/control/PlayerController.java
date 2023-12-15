2ea9600c54fa70efe0c03c3069b4c603d6333a63
turimaks <maximturianskyi@gmail.com>
2023-12-15 10:27:26 +0100

    PlayerController java.doc

--- src/main/java/my_project/control/PlayerController.java
+++ src/main/java/my_project/control/PlayerController.java
@@ -30,12 +30,27 @@ public class PlayerController{
         this.modifierController = modifierController;
     }
 
+    /**
+     * dashTimer counts down from 0
+     *
+     * If dashTimer is lower than 0
+     * Sets a new Player-Image
+     * Adds a slowingModifier
+     */
+
     public void update(double dt){
         dashTimer -= dt;
         if(dashTimer < 0)
             player.setDrawFirstImage(true);
         modifierController.add(new SlowingModifier(dt,bow.getPower() * 0.3));
     }
+
+    /**
+     * Moves the player and bow by the displacement amount
+     *
+     * @param xDisplacement X coordinate displacement
+     * @param yDisplacement Y coordinate displacement
+     */
 
     public void updatePlayerPosition(double xDisplacement, double yDisplacement){
         if(slowPercentage < 0.01)
@@ -46,14 +61,35 @@ public class PlayerController{
         bow.updateDesiredPosition(player.getX(),player.getY());
     }
 
+    /**
+     * Sets mouse Position on mouseX and mouseY
+     *
+     * @param mouseX X coordinate of the mouse
+     * @param mouseY Y coordinate of the mouse
+     */
+
     public void updateMousePosition(double mouseX, double mouseY){
         bow.setMousePosition(mouseX,mouseY);
     }
+
+    /**
+     * Sets left mouse button on isDown
+     *
+     * If the button is not down the bow shoots an Arrow
+     *
+     * @param isDown isDown stands for the state of a button (isDown or isUp)
+     */
 
     public void updateLeftMouseState(boolean isDown){
         bow.setLeftMouseDown(isDown);
         if(!isDown) shootArrow();
     }
+
+    /**
+     * Creates a new Arrow-Projectile from the shootInfo-array (gets the information from the Bow)
+     *
+     * Responsible for shaking the Screen
+     */
 
     private void shootArrow(){
         double[] shootInfo = bow.getShootInfo();
@@ -62,6 +98,18 @@ public class PlayerController{
         Arrow arrow = new Arrow(shootInfo[0],shootInfo[1],shootInfo[2],shootInfo[3] * 5000);
         spawnController.addProjectile(arrow);
     }
+
+    /**
+     * Checks whether the player can dash dependent on isDown
+     *
+     * If is not down the player is able to dash
+     *
+     * Else dashTimer and canDash get updated
+     * Adds a new AccelerationModifier
+     * Sets new Player-Image
+     *
+     * @param isDown isDown stands for the state of a button (isDown or isUp)
+     */
 
     public void updateRightMouseState(boolean isDown){
         if(!isDown)
